2880fc9d26527e7d61fb4a33f8760fb4075bd3aa
hoanthiennguyen <hoanthien97@gmail.com>
2024-08-04 13:11:51 +0700

    Write function to separate key val

--- src/deserializer_test.go
+++ src/deserializer_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"reflect"
 	"testing"
 )
 
@@ -62,7 +63,7 @@ func TestDeserailize(t *testing.T) {
 			want:    []int{1, 2, 3},
 		},
 		{
-			name: "array",
+			name: "struct",
 			args: args{
 				raw:  "{\"name\":\"John\",\"age\":30}",
 				dest: new(User),
@@ -71,6 +72,27 @@ func TestDeserailize(t *testing.T) {
 			want: User{
 				Name: "John",
 				Age:  30,
+			},
+		},
+		{
+			name: "nested struct",
+			args: args{
+				raw:  `{"name":"Math","students":[{"name":"John","age":30},{"name":"Doe","age":25}]}`,
+				dest: new(Class),
+			},
+			wantErr: false,
+			want: Class{
+				Name: "Math",
+				Students: []*User{
+					{
+						Name: "John",
+						Age:  30,
+					},
+					{
+						Name: "Doe",
+						Age:  25,
+					},
+				},
 			},
 		},
 	}
@@ -84,6 +106,75 @@ func TestDeserailize(t *testing.T) {
 			rawWant, _ := json.Marshal(tt.want)
 			if string(rawDest) != string(rawWant) {
 				t.Errorf("Deserailize dest: %s, want: %s", rawDest, rawWant)
+			}
+		})
+	}
+}
+
+func Test_separateKeyVal(t *testing.T) {
+	type args struct {
+		src string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    map[string]string
+		wantErr bool
+	}{
+		{
+			name: "struct",
+			args: args{
+				src: `{"name":"John","age":30}`,
+			},
+			want: map[string]string{
+				"name": `"John"`,
+				"age":  `30`,
+			},
+			wantErr: false,
+		},
+		{
+			name: "struct with array",
+			args: args{
+				src: `{"name":"John","age":[1,2,3]}`,
+			},
+			want: map[string]string{
+				"name": `"John"`,
+				"age":  `[1,2,3]`,
+			},
+			wantErr: false,
+		},
+		{
+			name: "struct with array string",
+			args: args{
+				src: `{"name":"John","age":["1","2"]}`,
+			},
+			want: map[string]string{
+				"name": `"John"`,
+				"age":  `["1","2"]`,
+			},
+			wantErr: false,
+		},
+		{
+			name: "nested struct",
+			args: args{
+				src: `{"name":"Math","students":[{"name":"John","age":30},{"name":"Doe","age":25}]}`,
+			},
+			want: map[string]string{
+				"name":     `"Math"`,
+				"students": `[{"name":"John","age":30},{"name":"Doe","age":25}]`,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := separateKeyVal(tt.args.src)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("separateKeyVal() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("separateKeyVal() = %v, want %v", got, tt.want)
 			}
 		})
 	}
